fc0cbf3f2c9ed7a8c594d33ee9095aabde7467ff
jacopower <119303726+jacopower@users.noreply.github.com>
2023-09-17 17:21:12 +0200

    13vector.cpp

--- uprog/13vector.cpp
+++ uprog/13vector.cpp
@@ -1,6 +1,7 @@
 // vector
 
 #include <iostream>
+#include <cmath>
 #include <vector>           // needed for vector
 
 using std::cin ;
@@ -29,40 +30,4 @@ int main () {   cout <<'\n' <<"Let's learn a bit about vectors..." <<'\n' <<'\n'
                 cout <<"v[0]=" <<v[0] <<", v[1]=" <<v[1] <<", v[2]=" <<v[2] <<", v[3]=" <<v[3] <<'\n' <<'\n' ;     // 2, 5, -2, 0
 
                 cout <<"v size=" <<v.size() <<'\n' <<'\n' ;               // method that gives the number of elements of the vector  // 3
-
-//
-
-                vector<int>::iterator first =v.begin() ;          // 'first' must be the same type as v.begin() = vector<int>::iterator type
-                                                                  // v.begin() is a method that points to the first element of vector v
-                int last = *v.end() ;                             // pointer to an iterator that points to one past the last element of the vector v
-             // i can use an iterator when i have a range (vector or string are ranges) by accessing it with the dot (range.iterator) or arrow (*range->iterator)
-
-             // cout <<first ;                  // error, the content of first must be passed as a pointer (because, as an iterator, has no value but has a content )
-                cout <<"first=" <<*first <<", last=" <<last ;       // 2, 7
-
-             // first = 3 ;                     // error, first in as iterator type, i cant change the value of its content this way...
-                v[0] = 3 ;                      // ...but i can this way (v is now {3,5,-2})
-                cout <<", new first=" <<*first <<'\n' <<'\n' ;         // 3
-             
-             // 'first' is the first container of information of v, not the value contained in it, so i can change the value and 'first' will point to it anyways
-
-
-                auto second = v.begin()+1 ;
-                
-                cout <<"(it)second=first = " << (first==second) <<'\n' ;       // 0
-                ++first ;                          // now it points to the next element
-                cout <<"(it)second=++first = " << (first==second) <<'\n' ;       // 1
-
-                --first ;
-                cout <<"first+2=third=" <<*(first+2) ;       //-2
-
-                first +=2 ;
-                cout <<" = " <<*first <<'\n' <<'\n' ;         // -2
-
-                v.erase(first) ;
-                cout <<"v[0]=" <<v[0] <<", v[1]=" <<v[1] <<", v[2]=" <<v[2] <<", v[3]=" <<v[3] <<'\n' ;     //
-
-                v.erase(second, v.end()) ;
-                cout <<"v[0]=" <<v[0] <<", v[1]=" <<v[1] <<", v[2]=" <<v[2] <<", v[3]=" <<v[3] <<'\n' <<'\n' ;     //
-
-            }
+   }
